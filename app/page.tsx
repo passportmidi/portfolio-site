da21393bf6e9a8b7f2de0849ec3bab9d437289e7
Sydney <69093420+passportmidi@users.noreply.github.com>
2025-05-01 17:37:59 -0300

    add header

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import { Header } from "../components/Header/Header";
 import { Hero } from "../components/Hero/Hero";
 
 export default function HomePage() {
   return (
     <div>
       <main>
-        <Hero></Hero>
+        <Header />
+        <Hero />
       </main>
     </div>
   );
