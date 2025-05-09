b796ff88653dfe2aa33937f7433893370c098dbf
Sydney <69093420+passportmidi@users.noreply.github.com>
2025-05-09 18:06:43 -0300

    connect blog RSS feed to article cards

--- components/Blog/Blog.tsx
+++ components/Blog/Blog.tsx
@@ -17,56 +17,71 @@ import {
 import { CardGrid } from "../CardGrid/CardGrid";
 import { useRouter } from "next/navigation";
 import classes from "./Blog.module.css";
+import { useState, useEffect } from "react";
+import Parser from "rss-parser";
+import dayjs from "dayjs";
 
 export function Blog() {
-  const linkProps = {
-    href: "https://mantine.dev",
-    target: "_blank",
-    rel: "noopener noreferrer",
-  };
   const theme = useMantineTheme();
   const router = useRouter();
+  const parser = new Parser();
+
+  const [articles, setArticles] = useState(null);
+
+  useEffect(() => {
+    const loadArticles = async () => {
+      const feed = await parser.parseURL(
+        "https://blog.putnam.computer/index.xml"
+      );
+      setArticles(feed);
+    };
+
+    loadArticles();
+  }, []);
+
+  if (!articles) {
+    return <div>loading...</div>;
+  }
+  const cards = articles.items.map((item) => (
+    <Card withBorder radius="md" className={classes.card} key={item.title}>
+      <Text
+        className={classes.title}
+        fz="lg"
+        fw={500}
+        ff="heading"
+        component="a"
+        href={item.link}
+      >
+        {item.title}
+      </Text>
+
+      <Text fz="sm" c="dimmed" lineClamp={4}>
+        {item.contentSnippet}
+      </Text>
+
+      <Group justify="space-between" className={classes.footer}>
+        <Center>
+          <Text fz="sm" c="var(--mantine-color-gray-6)" inline>
+            {dayjs(item.pubDate).format("MMMM D, YYYY")}
+          </Text>
+        </Center>
+
+        <Group gap={8} mr={0}>
+          <Button
+            radius="md"
+            style={{ flex: 1 }}
+            onClick={() => router.push(`${item.link}`)}
+          >
+            Read article
+          </Button>
+        </Group>
+      </Group>
+    </Card>
+  ));
 
   return (
     <CardGrid containerId="blog" title="Blog">
-      <Card withBorder radius="md" className={classes.card}>
-        <Text
-          className={classes.title}
-          fz="lg"
-          fw={500}
-          ff="heading"
-          component="a"
-          {...linkProps}
-        >
-          RecipeWizard
-        </Text>
-
-        <Text fz="sm" c="dimmed" lineClamp={4}>
-          Resident Evil Village is a direct sequel to 2017’s Resident Evil 7,
-          but takes a very different direction to its predecessor, namely the
-          fact that this time round instead of fighting against various mutated
-          zombies, you’re now dealing with more occult enemies like werewolves
-          and vampires.
-        </Text>
-
-        <Group justify="space-between" className={classes.footer}>
-          <Center>
-            <Text fz="sm" c="var(--mantine-color-gray-6)" inline>
-              May 9, 2025
-            </Text>
-          </Center>
-
-          <Group gap={8} mr={0}>
-            <Button
-              radius="md"
-              style={{ flex: 1 }}
-              onClick={() => router.push(`${linkProps.href}`)}
-            >
-              Read article
-            </Button>
-          </Group>
-        </Group>
-      </Card>
+      {cards}
     </CardGrid>
   );
 }
