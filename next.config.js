37f952e8c93a978445cc47a2fe3a54a93c8bb9ce
Sydney <69093420+passportmidi@users.noreply.github.com>
2025-05-23 22:19:56 -0300

    quick fix: fix contact form errors

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ module.exports = {
         source: "/api/blog",
         destination: "https://blog.putnam.computer/index.xml",
       },
+      {
+        source: "/api/email",
+        destination: "https://blog.putnam.computer/api/email",
+      },
     ];
   },
 };
