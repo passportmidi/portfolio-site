1c18dea95ed66ff2d8f332d3b5c706ebe6da787c
Sydney <69093420+passportmidi@users.noreply.github.com>
2025-05-23 22:28:24 -0300

    merge next.config.js into next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/blog",
+        destination: "https://blog.putnam.computer/index.xml",
+      },
+      {
+        source: "/api/email",
+        destination: "https://blog.putnam.computer/api/email",
+      },
+    ];
+  },
   experimental: {
     optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
   },
